932d93592c03724e894d147034bff9e5e612e73d
Bruno M. Mergen <brunomergen@gmail.com>
2018-01-14 13:30:18 -0200

    Adding shoulda matcher for user specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  before { @user = build(:user) }
+  subject { build(:user) }
 
-  it { expect(@user).to respond_to(:email) }
-  it { expect(@user).to respond_to(:name) }
-  it { expect(@user).to be_valid }
+  it { expect(subject).to respond_to(:email) }
+  it { expect(subject).to respond_to(:name) }
+  it { expect(subject).to be_valid }
+
+  describe 'validation' do
+    # it { expect(subject).to validate_presence_of(:name) }
+    # it { is_expected.to validate_presence_of(:name) }
+    it { should validate_presence_of(:name) }
+
+    it { should validate_presence_of(:email) }
+    it { should validate_uniqueness_of(:email).case_insensitive }
+    it { should allow_value('bruno@mergen.co').for(:email) }
+    it { should validate_confirmation_of(:password) }
+  end
 end
