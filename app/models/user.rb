dbda7b67db95113316b892c5cc6f7bf7d9d2005e
Bruno M. Mergen <brunomergen@gmail.com>
2018-01-15 22:38:08 -0200

    Adding auth_token to users and generating token in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,19 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-	validates :name, presence: true
+  validates :name, presence: true
+	validates :auth_token, uniqueness: true
+
+  before_create :generate_authentication_token!
+
+  def info
+    "#{email} - #{created_at} - Token: #{Devise.friendly_token}"
+  end
+
+  def generate_authentication_token!
+    self.auth_token = loop do
+      token = Devise.friendly_token
+      break token unless User.exists?(auth_token: token)
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,5 +16,32 @@ RSpec.describe User, type: :model do
     it { should validate_uniqueness_of(:email).case_insensitive }
     it { should allow_value('bruno@mergen.co').for(:email) }
     it { should validate_confirmation_of(:password) }
+    it { should validate_uniqueness_of(:auth_token) }
+
+    describe '#info' do
+      it "returns email and created_at and a Token" do
+        subject.save!
+        allow(Devise).to receive(:friendly_token).and_return('asd8u129eh91')
+        expect(subject.info).to eq("#{subject.email} - #{subject.created_at} - Token: asd8u129eh91")
+      end
+    end
+
+    describe '#generate_authentication_token!' do
+      it 'generates a unique auth_token' do
+        allow(Devise).to receive(:friendly_token).and_return('asd8u129eh91')
+        subject.generate_authentication_token!
+        expect(subject.auth_token).to eq('asd8u129eh91')
+      end
+      it 'generates another auth token when the current auth token already ha been taken' do
+        allow(Devise).to receive(:friendly_token).and_return('2983432923j98923', '2983432923j98923', 'abcuah8h7')
+        existing_user = create(:user)
+        subject.generate_authentication_token!
+        expect(subject.auth_token).to eq('abcuah8h7')
+      end
+      it 'will be called before creating' do
+        expect(subject).to receive(:generate_authentication_token!)
+        subject.run_callbacks(:create){ false }
+      end
+    end
   end
 end
